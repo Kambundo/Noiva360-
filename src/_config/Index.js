12079b0ab4743b210fab811aedf145cbf27c01a8
Kambundo <andrekambundo@gmail.com>
2025-01-10 18:02:35 +0000

    iuihuih

--- src/_config/Index.js
+++ src/_config/Index.js
@@ -5,18 +5,18 @@ const api = axios.create({
     
 })
 
-api.interceptors.response.use(undefined, (error) => {
-    if (error.message === 'Network Error' && error.code === 'ERR_NETWORK') {
-        console.log("Houve um erro no servidor")
-        console.log(error)
-        swal({
-            title: "Erro no Servidor" ,
-            text: "O servidor não está rodando",
-            icon: "error",
-            timer: "4000",
-            button: true,
-        });
-    }
-})
+// api.interceptors.response.use(undefined, (error) => {
+//     if (error.message === 'Network Error' && error.code === 'ERR_NETWORK') {
+//         console.log("Houve um erro no servidor")
+//         console.log(error)
+//         swal({
+//             title: "Erro no Servidor" ,
+//             text: "O servidor não está rodando",
+//             icon: "error",
+//             timer: "4000",
+//             button: true,
+//         });
+//     }
+// })
 
 export default api
